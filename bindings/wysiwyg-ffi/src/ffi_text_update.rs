19bde9b42fd9f606c5373d09c860f8c7c6fef353
Andy Balaam <andy.balaam@matrix.org>
2022-07-15 12:29:54 +0100

    Reflect enriched ReplaceAll structure in Android bindings

--- bindings/wysiwyg-ffi/src/ffi_text_update.rs
+++ bindings/wysiwyg-ffi/src/ffi_text_update.rs
@@ -1,15 +1,27 @@
 pub enum TextUpdate {
     Keep,
-    ReplaceAll { replacement_text: String },
+    ReplaceAll {
+        replacement_html: String,
+        selection_start_codepoint: u32,
+        selection_end_codepoint: u32,
+    },
 }
 
 impl TextUpdate {
     pub fn from(inner: wysiwyg::TextUpdate) -> Self {
         match inner {
             wysiwyg::TextUpdate::Keep => Self::Keep,
-            wysiwyg::TextUpdate::ReplaceAll(replacement_text) => {
-                Self::ReplaceAll { replacement_text }
-            }
+            wysiwyg::TextUpdate::ReplaceAll(replace_all) => Self::ReplaceAll {
+                replacement_html: replace_all.replacement_html,
+                selection_start_codepoint: u32::try_from(
+                    replace_all.selection_start_codepoint,
+                )
+                .unwrap(),
+                selection_end_codepoint: u32::try_from(
+                    replace_all.selection_end_codepoint,
+                )
+                .unwrap(),
+            },
         }
     }
 }
