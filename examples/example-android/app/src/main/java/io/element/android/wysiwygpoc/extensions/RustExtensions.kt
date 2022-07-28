52085494ed412fd618ca4d7d81096796f9a6efe0
Jorge Martín <jorgem@element.io>
2022-07-28 17:47:59 +0200

    [Android] Actually delete `examples/example-android*` modules.

--- examples/example-android/app/src/main/java/io/element/android/wysiwygpoc/extensions/RustExtensions.kt
+++
@@ -1,8 +0,0 @@
-package io.element.android.wysiwygpoc.extensions
-
-fun List<UShort>.string() = with(StringBuffer()) {
-    this@string.forEach {
-        appendCodePoint(it.toInt())
-    }
-    toString()
-}
