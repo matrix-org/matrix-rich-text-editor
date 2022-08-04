21fd5e6bf4d4bcd39394c446cf37a242c387dbec
aringenbach <arnaudr@element.io>
2022-08-04 16:54:07 +0200

    [iOS] Add unit test on empty content state

---
+++ platforms/ios/lib/WysiwygComposer/Tests/WysiwygComposerTests/WysiwygComposerViewModelTests.swift
@@ -0,0 +1,61 @@
+// 
+// Copyright 2022 The Matrix.org Foundation C.I.C
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+import XCTest
+import Combine
+@testable import WysiwygComposer
+
+final class WysiwygComposerViewModelTests: XCTestCase {
+    private let viewModel = WysiwygComposerViewModel()
+
+    func testIsContentEmpty() throws {
+        XCTAssertTrue(viewModel.isContentEmpty)
+
+        let expectFalse = self.expectation(description: "Await isContentEmpty false")
+        let cancellableFalse = viewModel.$isContentEmpty
+            // Ignore on subscribe publish.
+            .dropFirst()
+            .removeDuplicates()
+            .sink(receiveValue: { isEmpty in
+                XCTAssertFalse(isEmpty)
+                expectFalse.fulfill()
+            })
+
+        viewModel.replaceText(NSAttributedString(string: ""),
+                              range: .init(location: 0, length: 0),
+                              replacementText: "Test")
+
+        wait(for: [expectFalse], timeout: 2.0)
+        cancellableFalse.cancel()
+
+        let expectTrue = self.expectation(description: "Await isContentEmpty true")
+        let cancellableTrue = viewModel.$isContentEmpty
+            // Ignore on subscribe publish.
+            .dropFirst()
+            .removeDuplicates()
+            .sink(receiveValue: { isEmpty in
+                XCTAssertTrue(isEmpty)
+                expectTrue.fulfill()
+            })
+
+        viewModel.replaceText(viewModel.content.attributed,
+                              range: .init(location: 0, length: viewModel.content.attributed.length),
+                              replacementText: "")
+
+        wait(for: [expectTrue], timeout: 2.0)
+        cancellableTrue.cancel()
+    }
+}
