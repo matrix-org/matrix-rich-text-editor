1cce5cfe763ee4cc8f3e9ac31ee89044330acccc
Jorge Martín <jorgem@element.io>
2022-08-30 13:39:10 +0200

    Some code cleanup

--- crates/wysiwyg/src/node_joiner.rs
+++ crates/wysiwyg/src/node_joiner.rs
@@ -16,7 +16,7 @@ use std::marker::PhantomData;
 
 use crate::dom::nodes::{ContainerNodeKind, DomNode, TextNode};
 use crate::dom::{Dom, DomHandle, DomLocation, MultipleNodesRange, Range};
-use crate::{ToHtml, UnicodeString};
+use crate::UnicodeString;
 
 /// Handles joining together nodes after an edit event.
 ///
@@ -92,7 +92,7 @@ where
     }
 
     fn join_format_nodes(&self, dom: &mut Dom<S>, new_pos: usize) {
-        if let Some(mut next_node_handle) = Self::find_next_node(dom, new_pos) {
+        if let Some(next_node_handle) = Self::find_next_node(dom, new_pos) {
             self.join_format_nodes_at_level(dom, next_node_handle, 1);
         }
     }
@@ -222,7 +222,6 @@ where
                     if let DomNode::Container(parent) =
                         dom.lookup_node_mut(start_handle.parent_handle())
                     {
-                        let mut parent = parent;
                         parent.replace_child(
                             start_handle.index_in_parent(),
                             vec![text_node],
@@ -325,99 +324,6 @@ where
         ancestors
     }
 
-    fn join_nodes_of_same_type(dom: &mut Dom<S>, new_pos: usize) {
-        let new_range = dom.find_range(new_pos, new_pos);
-
-        if let Range::SameNode(new_range) = new_range {
-            // Find the text node we are in now the selection has been deleted
-            let current_text = new_range.node_handle;
-
-            // Find the container node that contains us e.g. <b>. This is
-            // what we are going to join with something else>
-            let current_container = current_text.parent_handle();
-
-            // Find the parent that contains both the <b> tags that we are
-            // going to join.
-            let parent = current_container.parent_handle();
-
-            // Look for our sibling container - e.g. the other <b>
-            // TODO: check we have a sibling instead of crashing here
-            let sibling =
-                parent.child_handle(current_container.index_in_parent() + 1);
-
-            Self::join_nodes_using_handles(dom, parent, current_text, sibling);
-        } else {
-            // We won't get here until we delete SameNode, or allow pasting
-            // HTML formatted text
-            panic!(
-                "Not set up for MultipleNodeRange, but we could use
-                   first_text_handle here to get the right node I think."
-            );
-        }
-    }
-
-    fn join_nodes_using_handles(
-        dom: &mut Dom<S>,
-        parent: DomHandle,
-        current_text: DomHandle,
-        sibling: DomHandle,
-    ) {
-        let sibling_node = dom.lookup_node(sibling.clone());
-        if let DomNode::Container(_) = sibling_node {
-            let sibling_text = sibling.child_handle(0);
-            let sibling_text_node = dom.lookup_node(sibling_text);
-            if let DomNode::Text(second_text_node) = sibling_text_node {
-                let remaining_text = second_text_node.data().clone();
-                Self::do_join_nodes(
-                    dom,
-                    parent,
-                    current_text,
-                    sibling,
-                    remaining_text,
-                );
-            } else {
-                // TODO: The first node in the sibling container was not text.
-                // We should probably just combine the children, but bailing
-                // out for now.
-            }
-        } else {
-            // TODO: Sibling is a text node - should probably join to
-            // it, but bailing out for now.
-        }
-    }
-
-    fn is_container(dom: &Dom<S>, handle: &DomHandle) -> bool {
-        matches!(dom.lookup_node(handle.clone()), DomNode::Container(_))
-    }
-
-    /// Actually join the nodes: delete the sibling, and append its text onto
-    /// our current text node.
-    fn do_join_nodes(
-        dom: &mut Dom<S>,
-        parent: DomHandle,
-        current_text: DomHandle,
-        sibling: DomHandle,
-        remaining_text: S,
-    ) {
-        // Delete the sibling's container
-        let parent_node = dom.lookup_node_mut(parent);
-        if let DomNode::Container(parent_node) = parent_node {
-            parent_node.remove_child(sibling.index_in_parent())
-        } else {
-            panic!("Parent was not a container!");
-        }
-
-        // Add the remaining text to this node
-        let current_text = dom.lookup_node_mut(current_text);
-        if let DomNode::Text(current_text) = current_text {
-            let mut new_data = current_text.data().clone();
-            new_data.push_string(&remaining_text);
-            current_text.set_data(new_data);
-        } else {
-            panic!("Current text was not text!");
-        }
-    }
-
     /// Search the supplied iterator for a text node and return a handle to it,
     /// or None if there are no text nodes.
     fn find_text_handle<'a>(
@@ -438,23 +344,5 @@ where
         range: &MultipleNodesRange,
     ) -> Option<DomHandle> {
         Self::find_text_handle(dom, range.locations.iter())
-    }
-
-    fn last_text_handle(
-        dom: &Dom<S>,
-        range: &MultipleNodesRange,
-    ) -> Option<DomHandle> {
-        Self::find_text_handle(dom, range.locations.iter().rev())
-    }
-
-    /// Panics if this handle does not refer to something with a parent
-    fn parent_container_type(dom: &Dom<S>, handle: DomHandle) -> String {
-        let parent = handle.parent_handle();
-        let node = dom.lookup_node(parent);
-        if let DomNode::Container(node) = node {
-            node.name().to_utf8()
-        } else {
-            panic!("Expected parent to be a container node");
-        }
     }
 }
