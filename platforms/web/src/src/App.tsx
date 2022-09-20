7d8ed72ff9d84e8609999005f665c87af4adc8d8
Florian Duros <florianduros@element.io>
2022-09-20 14:43:10 +0200

    Add formatting options

--- platforms/web/src/src/App.tsx
+++ platforms/web/src/src/App.tsx
@@ -1,4 +1,27 @@
+import { MouseEventHandler } from "react";
+
 import { useWysiwyg } from "./lib/useWysiwyg";
+import boldImage from './images/bold.svg';
+import undoImage from './images/undo.svg';
+import redoImage from './images/redo.svg';
+import italicImage from './images/italic.svg';
+import underlineImage from './images/underline.svg';
+import strikeTroughImage from './images/strike_through.svg';
+import listUnorderedImage from './images/list-unordered.svg';
+import listOrderedImage from './images/list-ordered.svg';
+
+type ButtonProps = {
+  onClick: MouseEventHandler<HTMLButtonElement>;
+  imagePath: string;
+  alt: string;
+};
+
+function Button({ onClick, imagePath, alt }: ButtonProps) {
+    return (
+        <button type="button" onClick={onClick}>
+            <img alt={alt} src={imagePath} />
+        </button>);
+}
 
 function App() {
     const { ref, isWysiwygReady, wysiwyg, debug } = useWysiwyg({ isAutoFocusEnabled: true });
@@ -7,10 +30,17 @@ function App() {
         <div className="wrapper">
             <div>
                 <div className="editor_container">
-                    <button type="button"
-                        onClick={(e) => wysiwyg.bold()}
-                    >bold
-                    </button>
+                    <div className="editor_toolbar">
+                        <Button onClick={wysiwyg.undo} alt="undo" imagePath={undoImage} />
+                        <Button onClick={wysiwyg.redo} alt="redo" imagePath={redoImage} />
+                        <Button onClick={wysiwyg.bold} alt="bold" imagePath={boldImage} />
+                        <Button onClick={wysiwyg.italic} alt="italic" imagePath={italicImage} />
+                        <Button onClick={wysiwyg.underline} alt="underline" imagePath={underlineImage} />
+                        <Button onClick={wysiwyg.strikeThrough} alt="strike through" imagePath={strikeTroughImage} />
+                        <Button onClick={wysiwyg.unorderedList} alt="list unordered" imagePath={listUnorderedImage} />
+                        <Button onClick={wysiwyg.orderedList} alt="list ordered" imagePath={listOrderedImage} />
+                        <button type="button">&lt;/&gt;</button>
+                    </div>
                     <div className="editor" ref={ref} contentEditable={isWysiwygReady} />
                 </div>
             </div>
--- platforms/web/src/src/lib/useWysiwyg.ts
+++ platforms/web/src/src/lib/useWysiwyg.ts
@@ -1,4 +1,4 @@
-import { RefObject, useEffect, useMemo, useRef, useState } from "react";
+import { MouseEvent as ReactMouseEvent, RefObject, useEffect, useMemo, useRef, useState } from "react";
 
 // rust generated bindings
 // eslint-disable-next-line camelcase
@@ -15,18 +15,43 @@ function useEditorFocus(editorRef: RefObject<HTMLElement | null>, isAutoFocusEna
         }
     }, [editorRef, isAutoFocusEnabled]);
 }
+/*
+const actions= {
+    bold: 'formatBold',
+    italic: 'formatItalic',
+    undo: 'historyUndo',
+    redo: 'historyRedo',
+
+};
+
+type FormattingActions = Record<keyof typeof actions, MouseEventHandler<HTMLElement>>;*/
 
 function useFormattingActions(editorRef: RefObject<HTMLElement | null>) {
-    const actions = useMemo(() => {
-        const sendInputEvent = (inputType: InputEvent['inputType']) =>
+    const formattingActions = useMemo(() => {
+        const sendInputEvent = (e: ReactMouseEvent<HTMLElement, MouseEvent>, inputType: InputEvent['inputType']) => {
+            e.preventDefault();
+            e.stopPropagation();
             editorRef.current?.dispatchEvent(new InputEvent('input', { inputType }));
+        };
+
+        /*return Object.keys(actions).reduce<FormattingActions>((acc, action) => {
+            acc[action] = (e: MouseEvent) => sendInputEvent(e, actions[action]);
+            return acc;
+        }, {} as FormattingActions);*/
 
         return {
-            bold: () => sendInputEvent('formatBold'),
+            bold: (e: ReactMouseEvent<HTMLElement, MouseEvent>) => sendInputEvent(e, 'formatBold'),
+            italic: (e: ReactMouseEvent<HTMLElement, MouseEvent>) => sendInputEvent(e, 'formatItalic'),
+            strikeThrough: (e: ReactMouseEvent<HTMLElement, MouseEvent>) => sendInputEvent(e, 'formatStrikeThrough'),
+            underline: (e: ReactMouseEvent<HTMLElement, MouseEvent>) => sendInputEvent(e, 'formatUnderline'),
+            undo: (e: ReactMouseEvent<HTMLElement, MouseEvent>) => sendInputEvent(e, 'historyUndo'),
+            redo: (e: ReactMouseEvent<HTMLElement, MouseEvent>) => sendInputEvent(e, 'historyRedo'),
+            orderedList: (e: ReactMouseEvent<HTMLElement, MouseEvent>) => sendInputEvent(e, 'insertOrderedList'),
+            unorderedList: (e: ReactMouseEvent<HTMLElement, MouseEvent>) => sendInputEvent(e, 'insertUnorderedList'),
         };
     }, [editorRef]);
 
-    return actions;
+    return formattingActions;
 }
 
 function useComposerModel() {
