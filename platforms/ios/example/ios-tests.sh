6ca5a081005ff4eddaa6e4c8aff546aa23e7c9b3
aringenbach <arnaudr@element.io>
2022-09-26 12:15:46 +0200

    [iOS] Run iOS tests on CI

---
+++ platforms/ios/example/ios-tests.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -eo pipefail
+
+xcodebuild \
+  -project Wysiwyg.xcodeproj \
+  -scheme Wysiwyg \
+  -sdk iphonesimulator \
+  -destination 'platform=iOS Simulator,name=iPhone 13,OS=15.5' \
+  -derivedDataPath ./DerivedData \
+  test
