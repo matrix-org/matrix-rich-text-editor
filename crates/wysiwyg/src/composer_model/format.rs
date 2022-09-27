72cd19b7a93ca56713690b28629945b69c2b2b4a
Jonas Platte <jplatte@matrix.org>
2022-09-27 12:31:53 +0200

    Update test cases

--- crates/wysiwyg/src/composer_model/format.rs
+++ crates/wysiwyg/src/composer_model/format.rs
@@ -492,7 +492,7 @@ mod test {
         model.format(InlineFormatType::Bold);
         assert_eq!(
             model.state.dom.to_string(),
-            "<strong>hello&nbsp;</strong><i><strong>wor</strong>ld</i>"
+            "<strong>hello\u{A0}</strong><i><strong>wor</strong>ld</i>"
         );
     }
 
