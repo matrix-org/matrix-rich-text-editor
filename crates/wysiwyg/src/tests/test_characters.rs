87a097f2b97e6f68550a7010ec53c4d2fd624ebd
Ivan Enderlin <ivan@mnt.io>
2022-09-28 11:43:10 +0200

    test: Move `#![cfg(test)]` on test module, rather than submodule.
    Instead of having `#![cfg(test)]` on all test files, we can put it
    directly in the `src/tests.rs` module.

--- crates/wysiwyg/src/tests/test_characters.rs
+++ crates/wysiwyg/src/tests/test_characters.rs
@@ -12,8 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#![cfg(test)]
-
 use widestring::Utf16String;
 
 use crate::tests::testutils_composer_model::{cm, restore_whitespace, tx};
