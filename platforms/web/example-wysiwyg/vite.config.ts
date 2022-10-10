f870f85a237e233a5368ca380e3fb2bcf16403a3
Andy Balaam <andy.balaam@matrix.org>
2022-10-10 11:59:54 +0100

    Disable eslint for generated files

--- platforms/web/example-wysiwyg/vite.config.ts
+++ platforms/web/example-wysiwyg/vite.config.ts
@@ -1,5 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+/* eslint-disable */
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
