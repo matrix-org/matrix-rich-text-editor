e79af808a7e65a2c2b3e7d9c5487c2caa22031a5
Jorge Martin Espinosa <jorgem@element.io>
2022-10-17 15:29:57 +0200

    [Android] Catch and print composer errors on release versions, crash in debug mode (#212)

--- platforms/android/library/src/main/java/io/element/android/wysiwyg/InputProcessor.kt
+++ platforms/android/library/src/main/java/io/element/android/wysiwyg/InputProcessor.kt
@@ -35,37 +35,45 @@ class InputProcessor(
     }
 
     fun processInput(action: EditorInputAction): TextUpdate? {
-        val update = when (action) {
-            is EditorInputAction.InsertText -> {
-                // This conversion to a plain String might be too simple
-                composer?.replaceText(action.value.toString())
-            }
-            is EditorInputAction.InsertParagraph -> {
-                composer?.enter()
-            }
-            is EditorInputAction.BackPress -> {
-                composer?.backspace()
-            }
-            is EditorInputAction.ApplyInlineFormat -> {
-                when (action.format) {
-                    InlineFormat.Bold -> composer?.bold()
-                    InlineFormat.Italic -> composer?.italic()
-                    InlineFormat.Underline -> composer?.underline()
-                    InlineFormat.StrikeThrough -> composer?.strikeThrough()
-                    InlineFormat.InlineCode -> composer?.inlineCode()
+        val update = runCatching {
+            when (action) {
+                is EditorInputAction.InsertText -> {
+                    // This conversion to a plain String might be too simple
+                    composer?.replaceText(action.value.toString())
+                }
+                is EditorInputAction.InsertParagraph -> {
+                    composer?.enter()
+                }
+                is EditorInputAction.BackPress -> {
+                    composer?.backspace()
+                }
+                is EditorInputAction.ApplyInlineFormat -> {
+                    when (action.format) {
+                        InlineFormat.Bold -> composer?.bold()
+                        InlineFormat.Italic -> composer?.italic()
+                        InlineFormat.Underline -> composer?.underline()
+                        InlineFormat.StrikeThrough -> composer?.strikeThrough()
+                        InlineFormat.InlineCode -> composer?.inlineCode()
+                    }
+                }
+                is EditorInputAction.Delete -> {
+                    composer?.deleteIn(action.start.toUInt(), action.end.toUInt())
+                }
+                is EditorInputAction.SetLink -> composer?.setLink(action.link)
+                is EditorInputAction.ReplaceAllHtml -> composer?.replaceAllHtml(action.html)
+                is EditorInputAction.Undo -> composer?.undo()
+                is EditorInputAction.Redo -> composer?.redo()
+                is EditorInputAction.ToggleList -> {
+                    if (action.ordered) composer?.orderedList() else composer?.unorderedList()
                 }
             }
-            is EditorInputAction.Delete -> {
-                composer?.deleteIn(action.start.toUInt(), action.end.toUInt())
+        }.onFailure {
+            if (BuildConfig.DEBUG) {
+                throw it
+            } else {
+                it.printStackTrace()
             }
-            is EditorInputAction.SetLink -> composer?.setLink(action.link)
-            is EditorInputAction.ReplaceAllHtml -> composer?.replaceAllHtml(action.html)
-            is EditorInputAction.Undo -> composer?.undo()
-            is EditorInputAction.Redo -> composer?.redo()
-            is EditorInputAction.ToggleList -> {
-                if (action.ordered) composer?.orderedList() else composer?.unorderedList()
-            }
-        }
+        }.getOrNull()
 
         update?.menuState()?.let { menuStateCallback(it) }
 
