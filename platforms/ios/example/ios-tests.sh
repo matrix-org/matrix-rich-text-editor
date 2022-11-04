79a0d121237e3eb809d85806ac368b79cddfdb5b
aringenbach <arnaudr@element.io>
2022-11-04 17:29:30 +0100

    [iOS] Update simulator destination

--- platforms/ios/example/ios-tests.sh
+++ platforms/ios/example/ios-tests.sh
@@ -6,6 +6,6 @@ xcodebuild \
   -project Wysiwyg.xcodeproj \
   -scheme Wysiwyg \
   -sdk iphonesimulator \
-  -destination 'platform=iOS Simulator,name=iPhone 13,OS=15.5' \
+  -destination 'platform=iOS Simulator,name=iPhone 14,OS=16.1' \
   -derivedDataPath ./DerivedData \
   test
