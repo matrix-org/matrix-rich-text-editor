7bbe9c79c2b5e2383ecf80b916c809920d6f0901
Jorge Martín <jorgem@element.io>
2022-11-11 12:01:44 +0100

    Added update version script

---
+++ update_version.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+if (($# != 1)); then
+  echo "There should be a single version argument passed."
+  exit 1
+fi
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  if ! command -v gsed &> /dev/null; then
+    echo "GNU-SED not found. Please install it using `brew install gnu-sed`."
+    exit 1
+  fi
+  SED_CMD='gsed -i'
+else
+  SED_CMD="sed -i"
+fi
+
+VERSION=$1
+
+echo "Updating Rust"
+$SED_CMD "s/^version\s*=\s*\".*\"/version = \"$VERSION\"/g" bindings/wysiwyg-ffi/Cargo.toml
+$SED_CMD "s/^version\s*=\s*\".*\"/version = \"$VERSION\"/g" bindings/wysiwyg-wasm/Cargo.toml
+$SED_CMD "s/^version\s*=\s*\".*\"/version = \"$VERSION\"/g" crates/wysiwyg/Cargo.toml
+$SED_CMD "s/\"version\":\s*\".+\"/\"version\": \"$VERSION\"/g" bindings/wysiwyg-wasm/package.json
+
+echo "Updating Web"
+$SED_CMD "s/\"version\":\s*\".+\"/\"version\": \"$VERSION\"/g" platforms/web/package.json
+
+echo "Updating Android"
+$SED_CMD "s/version\s*=\s*\".*\"/version = \"$VERSION\"/g" platforms/android/publish.gradle
