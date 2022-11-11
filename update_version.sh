a196be4575ac925d8ba49e258a723a45d2ac01d9
Jorge Martin Espinosa <jorgem@element.io>
2022-11-11 16:05:18 +0100

    Restore previous approach to Android publish config (#292)
    * Restore `gradle.properties` approach for Android versioning.
    
    * Update `update_version.sh`, make it work from any dir.

--- update_version.sh
+++ update_version.sh
@@ -15,19 +15,21 @@ else
   SED_CMD="sed -i"
 fi
 
+SCRIPT_PATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+
 VERSION=$1
 CARGO_REGEX="s/^version\s*=\s*\".*\"/version = \"$VERSION\"/g"
 PACKAGE_JSON_REGEX="s/\"version\":\s*\".*\"/\"version\": \"$VERSION\"/g"
-GRADLE_REGEX="s/^version\s*=\s*\".*\"/version = \"$VERSION\"/g"
+GRADLE_PROPERTIES_REGEX="s/^VERSION_NAME=.*$/VERSION_NAME=$VERSION/g"
 
 echo "Updating Rust"
-$SED_CMD "$CARGO_REGEX" bindings/wysiwyg-ffi/Cargo.toml
-$SED_CMD "$CARGO_REGEX" bindings/wysiwyg-wasm/Cargo.toml
-$SED_CMD "$CARGO_REGEX" crates/wysiwyg/Cargo.toml
+$SED_CMD "$CARGO_REGEX" $SCRIPT_PATH/bindings/wysiwyg-ffi/Cargo.toml
+$SED_CMD "$CARGO_REGEX" $SCRIPT_PATH/bindings/wysiwyg-wasm/Cargo.toml
+$SED_CMD "$CARGO_REGEX" $SCRIPT_PATH/crates/wysiwyg/Cargo.toml
 
 echo "Updating Web"
-$SED_CMD "$PACKAGE_JSON_REGEX" platforms/web/package.json
-$SED_CMD "$PACKAGE_JSON_REGEX" bindings/wysiwyg-wasm/package.json
+$SED_CMD "$PACKAGE_JSON_REGEX" $SCRIPT_PATH/platforms/web/package.json
+$SED_CMD "$PACKAGE_JSON_REGEX" $SCRIPT_PATH/bindings/wysiwyg-wasm/package.json
 
 echo "Updating Android"
-$SED_CMD "$GRADLE_REGEX" platforms/android/publish.gradle
+$SED_CMD "$GRADLE_PROPERTIES_REGEX" $SCRIPT_PATH/platforms/android/gradle.properties
