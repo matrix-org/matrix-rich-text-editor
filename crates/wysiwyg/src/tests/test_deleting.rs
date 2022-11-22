f6eb1a8c29ded9b2f794107648a93be03acd473d
Alun Turner <alunt@element.io>
2022-11-22 11:43:15 +0000

    uncomment tests

--- crates/wysiwyg/src/tests/test_deleting.rs
+++ crates/wysiwyg/src/tests/test_deleting.rs
@@ -17,259 +17,259 @@ use crate::{
     ComposerModel, TextUpdate,
 };
 
-// #[test]
-// fn backspacing_a_character_at_the_end_deletes_it() {
-//     let mut model = cm("abc|");
-//     model.backspace();
-//     assert_eq!(tx(&model), "ab|");
-// }
+#[test]
+fn backspacing_a_character_at_the_end_deletes_it() {
+    let mut model = cm("abc|");
+    model.backspace();
+    assert_eq!(tx(&model), "ab|");
+}
 
-// #[test]
-// fn backspacing_a_character_at_the_beginning_does_nothing() {
-//     let mut model = cm("|abc");
-//     model.backspace();
-//     assert_eq!(tx(&model), "|abc");
-// }
+#[test]
+fn backspacing_a_character_at_the_beginning_does_nothing() {
+    let mut model = cm("|abc");
+    model.backspace();
+    assert_eq!(tx(&model), "|abc");
+}
 
-// #[test]
-// fn backspacing_a_character_in_the_middle_deletes_it() {
-//     let mut model = cm("ab|c");
-//     model.backspace();
-//     assert_eq!(tx(&model), "a|c");
-// }
+#[test]
+fn backspacing_a_character_in_the_middle_deletes_it() {
+    let mut model = cm("ab|c");
+    model.backspace();
+    assert_eq!(tx(&model), "a|c");
+}
 
-// #[test]
-// fn backspacing_a_selection_deletes_it() {
-//     let mut model = cm("a{bc}|");
-//     model.backspace();
-//     assert_eq!(tx(&model), "a|");
-// }
+#[test]
+fn backspacing_a_selection_deletes_it() {
+    let mut model = cm("a{bc}|");
+    model.backspace();
+    assert_eq!(tx(&model), "a|");
+}
 
-// #[test]
-// fn backspacing_a_backwards_selection_deletes_it() {
-//     let mut model = cm("a|{bc}");
-//     model.backspace();
-//     assert_eq!(tx(&model), "a|");
-// }
+#[test]
+fn backspacing_a_backwards_selection_deletes_it() {
+    let mut model = cm("a|{bc}");
+    model.backspace();
+    assert_eq!(tx(&model), "a|");
+}
 
-// #[test]
-// fn backspacing_a_lone_newline_deletes_it() {
-//     let mut model = ComposerModel::new();
-//     model.enter();
-//     model.backspace();
-//     assert_eq!(tx(&model), "|");
-// }
+#[test]
+fn backspacing_a_lone_newline_deletes_it() {
+    let mut model = ComposerModel::new();
+    model.enter();
+    model.backspace();
+    assert_eq!(tx(&model), "|");
+}
 
-// #[test]
-// fn backspacing_a_newline_deletes_it() {
-//     let mut model = cm("abc|");
-//     let update = model.enter();
+#[test]
+fn backspacing_a_newline_deletes_it() {
+    let mut model = cm("abc|");
+    let update = model.enter();
 
-//     let replace_all = match update.text_update {
-//         TextUpdate::Keep => panic!("expected ReplaceAll"),
-//         TextUpdate::ReplaceAll(replace_all) => replace_all,
-//         TextUpdate::Select(_) => panic!("expected ReplaceAll"),
-//     };
+    let replace_all = match update.text_update {
+        TextUpdate::Keep => panic!("expected ReplaceAll"),
+        TextUpdate::ReplaceAll(replace_all) => replace_all,
+        TextUpdate::Select(_) => panic!("expected ReplaceAll"),
+    };
 
-//     assert_eq!(replace_all.start, 4);
-//     assert_eq!(replace_all.end, 4);
+    assert_eq!(replace_all.start, 4);
+    assert_eq!(replace_all.end, 4);
 
-//     model.backspace();
-//     model.backspace();
-//     assert_eq!(tx(&model), "ab|");
-// }
+    model.backspace();
+    model.backspace();
+    assert_eq!(tx(&model), "ab|");
+}
 
-// #[test]
-// fn deleting_a_character_at_the_end_does_nothing() {
-//     let mut model = cm("abc|");
-//     model.delete();
-//     assert_eq!(tx(&model), "abc|");
-// }
+#[test]
+fn deleting_a_character_at_the_end_does_nothing() {
+    let mut model = cm("abc|");
+    model.delete();
+    assert_eq!(tx(&model), "abc|");
+}
 
-// #[test]
-// fn deleting_a_character_at_the_beginning_deletes_it() {
-//     let mut model = cm("|abc");
-//     model.delete();
-//     assert_eq!(tx(&model), "|bc");
-// }
+#[test]
+fn deleting_a_character_at_the_beginning_deletes_it() {
+    let mut model = cm("|abc");
+    model.delete();
+    assert_eq!(tx(&model), "|bc");
+}
 
-// #[test]
-// fn deleting_a_character_in_the_middle_deletes_it() {
-//     let mut model = cm("a|bc");
-//     model.delete();
-//     assert_eq!(tx(&model), "a|c");
-// }
+#[test]
+fn deleting_a_character_in_the_middle_deletes_it() {
+    let mut model = cm("a|bc");
+    model.delete();
+    assert_eq!(tx(&model), "a|c");
+}
 
-// #[test]
-// fn deleting_a_selection_deletes_it() {
-//     let mut model = cm("a{bc}|");
-//     model.delete();
-//     assert_eq!(tx(&model), "a|");
-// }
+#[test]
+fn deleting_a_selection_deletes_it() {
+    let mut model = cm("a{bc}|");
+    model.delete();
+    assert_eq!(tx(&model), "a|");
+}
 
-// #[test]
-// fn deleting_a_backwards_selection_deletes_it() {
-//     let mut model = cm("a|{bc}");
-//     model.delete();
-//     assert_eq!(tx(&model), "a|");
-// }
+#[test]
+fn deleting_a_backwards_selection_deletes_it() {
+    let mut model = cm("a|{bc}");
+    model.delete();
+    assert_eq!(tx(&model), "a|");
+}
 
-// #[test]
-// fn deleting_a_range_removes_it() {
-//     let mut model = cm("abcd|");
-//     model.delete_in(1, 3);
-//     assert_eq!(tx(&model), "a|d");
-// }
+#[test]
+fn deleting_a_range_removes_it() {
+    let mut model = cm("abcd|");
+    model.delete_in(1, 3);
+    assert_eq!(tx(&model), "a|d");
+}
 
-// #[test]
-// fn deleting_when_spanning_two_separate_identical_tags_joins_them() {
-//     let mut model = cm("<b>bo{ld</b> plain <b>BO}|LD</b>");
-//     model.delete();
-//     assert_eq!(tx(&model), "<b>bo|LD</b>");
-// }
+#[test]
+fn deleting_when_spanning_two_separate_identical_tags_joins_them() {
+    let mut model = cm("<b>bo{ld</b> plain <b>BO}|LD</b>");
+    model.delete();
+    assert_eq!(tx(&model), "<b>bo|LD</b>");
+}
 
-// #[test]
-// fn deleting_across_list_items_joins_them() {
-//     let mut model = cm("<ol>
-//             <li>1{1</li>
-//             <li>22</li>
-//             <li>33</li>
-//             <li>4}|4</li>
-//         </ol>");
-//     model.delete();
-//     assert_eq!(
-//         restore_whitespace(&tx(&model)),
-//         "<ol>
-//             <li>1|4</li>
-//         </ol>"
-//     );
-// }
+#[test]
+fn deleting_across_list_items_joins_them() {
+    let mut model = cm("<ol>
+            <li>1{1</li>
+            <li>22</li>
+            <li>33</li>
+            <li>4}|4</li>
+        </ol>");
+    model.delete();
+    assert_eq!(
+        restore_whitespace(&tx(&model)),
+        "<ol>
+            <li>1|4</li>
+        </ol>"
+    );
+}
 
-// #[test]
-// fn deleting_across_lists_joins_them() {
-//     let mut model = cm("<ol>
-//             <li>1{1</li>
-//             <li>22</li>
-//         </ol>
-//         <ol>
-//             <li>33</li>
-//             <li>4}|4</li>
-//         </ol>");
-//     model.delete();
-//     assert_eq!(
-//         restore_whitespace(&tx(&model)),
-//         "<ol>
-//             <li>1|4</li>
-//         </ol>"
-//     );
-// }
+#[test]
+fn deleting_across_lists_joins_them() {
+    let mut model = cm("<ol>
+            <li>1{1</li>
+            <li>22</li>
+        </ol>
+        <ol>
+            <li>33</li>
+            <li>4}|4</li>
+        </ol>");
+    model.delete();
+    assert_eq!(
+        restore_whitespace(&tx(&model)),
+        "<ol>
+            <li>1|4</li>
+        </ol>"
+    );
+}
 
-// #[test]
-// fn deleting_across_lists_joins_them_nested() {
-//     let mut model = cm("<ol>
-//             <li>1{1</li>
-//             <li>22</li>
-//             <ol>
-//                 <li>55</li>
-//             </ol>
-//         </ol>
-//         <ol>
-//             <li>33</li>
-//             <li>4}|4</li>
-//         </ol>");
-//     model.delete();
-//     assert_eq!(
-//         restore_whitespace(&tx(&model)),
-//         "<ol>
-//             <li>1|4</li>
-//         </ol>"
-//     );
-// }
+#[test]
+fn deleting_across_lists_joins_them_nested() {
+    let mut model = cm("<ol>
+            <li>1{1</li>
+            <li>22</li>
+            <ol>
+                <li>55</li>
+            </ol>
+        </ol>
+        <ol>
+            <li>33</li>
+            <li>4}|4</li>
+        </ol>");
+    model.delete();
+    assert_eq!(
+        restore_whitespace(&tx(&model)),
+        "<ol>
+            <li>1|4</li>
+        </ol>"
+    );
+}
 
-// #[test]
-// fn deleting_across_formatting_different_types() {
-//     let mut model = cm("<b><i>some {italic</i></b> and}| <b>bold</b> text");
-//     model.delete();
-//     assert_eq!(tx(&model), "<b><i>some&nbsp;|</i></b> <b>bold</b> text");
-// }
+#[test]
+fn deleting_across_formatting_different_types() {
+    let mut model = cm("<b><i>some {italic</i></b> and}| <b>bold</b> text");
+    model.delete();
+    assert_eq!(tx(&model), "<b><i>some&nbsp;|</i></b> <b>bold</b> text");
+}
 
-// #[test]
-// fn deleting_across_formatting_different_types_on_node_boundary() {
-//     let mut model = cm("<b><i>some {italic</i></b> and }|<b>bold</b> text");
-//     model.delete();
-//     assert_eq!(tx(&model), "<b><i>some&nbsp;|</i>bold</b> text");
-// }
+#[test]
+fn deleting_across_formatting_different_types_on_node_boundary() {
+    let mut model = cm("<b><i>some {italic</i></b> and }|<b>bold</b> text");
+    model.delete();
+    assert_eq!(tx(&model), "<b><i>some&nbsp;|</i>bold</b> text");
+}
 
-// #[test]
-// fn deleting_in_nested_structure_and_format_nodes_works() {
-//     let mut model = cm("<ul><li>A</li><li><b>B{B</b><b>C}|C</b></li></ul>");
-//     model.delete();
-//     assert_eq!(tx(&model), "<ul><li>A</li><li><b>B|C</b></li></ul>");
-// }
+#[test]
+fn deleting_in_nested_structure_and_format_nodes_works() {
+    let mut model = cm("<ul><li>A</li><li><b>B{B</b><b>C}|C</b></li></ul>");
+    model.delete();
+    assert_eq!(tx(&model), "<ul><li>A</li><li><b>B|C</b></li></ul>");
+}
 
-// #[test]
-// #[ignore] // TODO: fix this test once this deletion works
-// fn deleting_empty_list_item() {
-//     let mut model = cm("<ul><li>A{</li><li>~}|</li></ul>");
-//     model.backspace();
-//     assert_eq!(tx(&model), "<ul><li>A|</li></ul>");
-// }
+#[test]
+#[ignore] // TODO: fix this test once this deletion works
+fn deleting_empty_list_item() {
+    let mut model = cm("<ul><li>A{</li><li>~}|</li></ul>");
+    model.backspace();
+    assert_eq!(tx(&model), "<ul><li>A|</li></ul>");
+}
 
-// #[test]
-// fn deleting_a_newline_deletes_it() {
-//     let mut model = cm("abc|<br />def");
-//     model.delete();
-//     model.delete();
-//     assert_eq!(tx(&model), "abc|ef");
-// }
+#[test]
+fn deleting_a_newline_deletes_it() {
+    let mut model = cm("abc|<br />def");
+    model.delete();
+    model.delete();
+    assert_eq!(tx(&model), "abc|ef");
+}
 
-// #[test]
-// fn test_backspace_emoji() {
-//     let mut model = cm("😄|😅");
-//     model.backspace();
-//     assert_eq!(tx(&model), "|😅");
-// }
+#[test]
+fn test_backspace_emoji() {
+    let mut model = cm("😄|😅");
+    model.backspace();
+    assert_eq!(tx(&model), "|😅");
+}
 
-// #[test]
-// fn test_backspace_complex_emoji() {
-//     let mut model = cm("Test😮‍💨|😅");
-//     model.backspace();
-//     assert_eq!(tx(&model), "Test|😅");
-//     model.select(6.into(), 6.into());
-//     model.backspace();
-//     assert_eq!(tx(&model), "Test|");
-// }
+#[test]
+fn test_backspace_complex_emoji() {
+    let mut model = cm("Test😮‍💨|😅");
+    model.backspace();
+    assert_eq!(tx(&model), "Test|😅");
+    model.select(6.into(), 6.into());
+    model.backspace();
+    assert_eq!(tx(&model), "Test|");
+}
 
-// #[test]
-// fn test_delete_emoji() {
-//     let mut model = cm("😄|😅");
-//     model.delete();
-//     assert_eq!(tx(&model), "😄|");
-// }
+#[test]
+fn test_delete_emoji() {
+    let mut model = cm("😄|😅");
+    model.delete();
+    assert_eq!(tx(&model), "😄|");
+}
 
-// #[test]
-// fn test_delete_complex_emoji() {
-//     let mut model = cm("Test😮‍💨|😅");
-//     model.delete();
-//     assert_eq!(tx(&model), "Test😮‍💨|");
-//     model.select(4.into(), 4.into());
-//     model.delete();
-//     assert_eq!(tx(&model), "Test|");
-// }
+#[test]
+fn test_delete_complex_emoji() {
+    let mut model = cm("Test😮‍💨|😅");
+    model.delete();
+    assert_eq!(tx(&model), "Test😮‍💨|");
+    model.select(4.into(), 4.into());
+    model.delete();
+    assert_eq!(tx(&model), "Test|");
+}
 
-// #[test]
-// fn test_delete_complex_grapheme() {
-//     let mut model = cm("Test|О́");
-//     model.delete();
-//     assert_eq!(tx(&model), "Test|");
-// }
+#[test]
+fn test_delete_complex_grapheme() {
+    let mut model = cm("Test|О́");
+    model.delete();
+    assert_eq!(tx(&model), "Test|");
+}
 
-// #[test]
-// fn test_backspace_complex_grapheme() {
-//     let mut model = cm("TestО́|");
-//     model.backspace();
-//     assert_eq!(tx(&model), "Test|");
-// }
+#[test]
+fn test_backspace_complex_grapheme() {
+    let mut model = cm("TestО́|");
+    model.backspace();
+    assert_eq!(tx(&model), "Test|");
+}
 
 #[test]
 fn backspace_word_at_beginning_does_nothing(){
