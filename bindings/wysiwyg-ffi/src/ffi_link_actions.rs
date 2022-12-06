70a0e5074f106604e7eafc8e78df7e18527116de
jonnyandrew <jonny.andrew@protonmail.com>
2022-12-06 17:48:07 +0000

    Change link interface to use string (#380)
    Update the interface for link actions to use `String` instead of `Vec<u16>` so that the Kotlin/Swift bindings generated for this class use the native string types and do not need conversion.

--- bindings/wysiwyg-ffi/src/ffi_link_actions.rs
+++ bindings/wysiwyg-ffi/src/ffi_link_actions.rs
@@ -3,7 +3,7 @@ use widestring::Utf16String;
 pub enum LinkAction {
     CreateWithText,
     Create,
-    Edit { link: Vec<u16> },
+    Edit { link: String },
 }
 
 impl From<wysiwyg::LinkAction<Utf16String>> for LinkAction {
@@ -12,7 +12,7 @@ impl From<wysiwyg::LinkAction<Utf16String>> for LinkAction {
             wysiwyg::LinkAction::CreateWithText => Self::CreateWithText,
             wysiwyg::LinkAction::Create => Self::Create,
             wysiwyg::LinkAction::Edit(link) => Self::Edit {
-                link: link.into_vec(),
+                link: link.to_string(),
             },
         }
     }
