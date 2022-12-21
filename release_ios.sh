87b592f65ca9672278c5113057177a063151bd8e
Mauro Romito <mauro.romito@element.io>
2022-12-21 14:52:57 +0100

    using https should solve the issue

--- release_ios.sh
+++ release_ios.sh
@@ -37,7 +37,7 @@ REPO_PATH="${BUILD_DIR}/matrix-wysiwyg-composer-swift"
 WYSIWYG_COMPOSER_PATH="platforms/ios/lib/WysiwygComposer/"
 rm -rf $BUILD_DIR
 mkdir $BUILD_DIR
-git clone git@github.com:matrix-org/matrix-wysiwyg-composer-swift.git $REPO_PATH
+git clone https://matrix-org/matrix-wysiwyg-composer-swift.git $REPO_PATH
 git checkout main
 rsync -a --delete --exclude=".git" $WYSIWYG_COMPOSER_PATH $REPO_PATH
 last_commit=$(git rev-parse --short HEAD);
