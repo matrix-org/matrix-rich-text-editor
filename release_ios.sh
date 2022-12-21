47948a31e8f967f4af6e733e566d2b469c7f9b6d
Mauro Romito <mauro.romito@element.io>
2022-12-21 14:44:10 +0100

    git checkout

--- release_ios.sh
+++ release_ios.sh
@@ -38,7 +38,7 @@ WYSIWYG_COMPOSER_PATH="platforms/ios/lib/WysiwygComposer/"
 rm -rf $BUILD_DIR
 mkdir $BUILD_DIR
 git clone git@github.com:matrix-org/matrix-wysiwyg-composer-swift.git $REPO_PATH
-# git checkout main
+git checkout main
 rsync -a --delete --exclude=".git" $WYSIWYG_COMPOSER_PATH $REPO_PATH
 last_commit=$(git rev-parse --short HEAD);
 RELEASE_BRANCH="release_$last_commit"
