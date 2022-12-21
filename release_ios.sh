782b2275141fd10879efca455e013f39220edb64
Mauro Romito <mauro.romito@element.io>
2022-12-21 12:54:14 +0100

    test

--- release_ios.sh
+++ release_ios.sh
@@ -56,4 +56,4 @@ else
 fi
 git push origin $BRANCH_NAME
 # if you have github cli installed you can also create the PR automatically
-gh pr create -f
+gh pr create -B main -t $BRANCH_NAME -b $RELEASE_BRANCH
