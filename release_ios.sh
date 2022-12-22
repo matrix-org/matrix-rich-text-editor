a101a91a4001b3b269989631249aa7dec5e623bd
Velin92 <34335419+Velin92@users.noreply.github.com>
2022-12-22 10:22:32 +0100

    [iOS] RTE release workflow to open a Swift PM repo release PR (#422)
    * test
    
    * manual
    
    * workflow_dispatch
    
    * push
    
    * FOR TESTING ONLY REVERT THIS
    
    * improved sh
    
    * REVERT THIS ONE TOO
    
    * improved
    
    * check
    
    * improv
    
    * trying again
    
    * improvement on the bash
    
    * bash improv
    
    * commit
    
    * testing
    
    * tag is set
    
    * removed the var
    
    * tag check
    
    * improved check
    
    * HAS TAG = 0
    
    * test
    
    * automated pr open
    
    * test again
    
    * tag improvement
    
    * TEST
    
    * check gh pr create
    
    * gh_token
    
    * test
    
    * right remote
    
    * GH_TOKEN is using the secret from SWIFTPM
    
    * this should work
    
    * fix and improvement
    
    * git checkout
    
    * using https should solve the issue
    
    * fixed the link
    
    * added a git fetch
    
    * some echos
    
    * setting my username
    
    * setting credentials on the workflow
    
    * env added back
    
    * removing unnecessary code
    
    * polished the code
    
    * automatically setting reviewers
    
    * done
    
    * renaming the job

--- release_ios.sh
+++ release_ios.sh
@@ -1,5 +1,17 @@
 #!/usr/bin/env bash
 
+TAG=""
+
+while getopts ":t:" option; do
+   case "${option}" in
+      t) # Enter a name
+          TAG=${OPTARG};;
+     \?) # Invalid option
+          echo "Error: Invalid option"
+          exit;;
+   esac
+done
+
 BRANCH_NAME=$(git rev-parse --abbrev-ref HEAD)
 
 if [ $BRANCH_NAME == "main" ]; then 
@@ -24,7 +36,8 @@ REPO_PATH="${BUILD_DIR}/matrix-wysiwyg-composer-swift"
 WYSIWYG_COMPOSER_PATH="platforms/ios/lib/WysiwygComposer/"
 rm -rf $BUILD_DIR
 mkdir $BUILD_DIR
-git clone git@github.com:matrix-org/matrix-wysiwyg-composer-swift.git $REPO_PATH
+git clone https://github.com/matrix-org/matrix-wysiwyg-composer-swift.git $REPO_PATH
+git fetch
 git checkout main
 rsync -a --delete --exclude=".git" $WYSIWYG_COMPOSER_PATH $REPO_PATH
 last_commit=$(git rev-parse --short HEAD);
@@ -33,4 +46,14 @@ cd $REPO_PATH
 git checkout -b $RELEASE_BRANCH
 git add .
 git commit -m "release $last_commit"
-git push origin $RELEASE_BRANCH
+if [ "$TAG" != "" ]; then
+  echo "found a tag $TAG"]
+  REMOTE_BRANCH_NAME="release_$TAG"
+  git tag $TAG
+else
+  echo "tag not found"
+  REMOTE_BRANCH_NAME=${RELEASE_BRANCH}
+fi
+git push origin $RELEASE_BRANCH:$REMOTE_BRANCH_NAME
+# if you have github cli installed this will create the PR automatically
+gh pr create -r matrix.org/professional-services-uk -f
