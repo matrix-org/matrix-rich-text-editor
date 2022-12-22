ccb66f65ec0c454901db5c2477e5396508821c77
Velin92 <34335419+Velin92@users.noreply.github.com>
2022-12-22 11:01:57 +0100

    Update RELEASE.md (#425)
    * Update RELEASE.md
    
    * updating tests
    
    * release md update
    
    * improved

--- platforms/ios/example/ios-tests.sh
+++ platforms/ios/example/ios-tests.sh
@@ -6,6 +6,6 @@ xcodebuild \
   -project Wysiwyg.xcodeproj \
   -scheme Wysiwyg \
   -sdk iphonesimulator \
-  -destination 'platform=iOS Simulator,name=iPhone 14,OS=16.1' \
+  -destination 'platform=iOS Simulator,name=iPhone 14,OS=16.2' \
   -derivedDataPath ./DerivedData \
   test
