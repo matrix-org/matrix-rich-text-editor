22dd003fbd5afd7387f88d736b97528d9699b659
Alun Turner <alunt@element.io>
2022-12-23 15:21:14 +0000

    rename Dal to ActionList

--- crates/wysiwyg/src/dom/action_list.rs
+++ crates/wysiwyg/src/dom/action_list.rs
@@ -195,31 +195,31 @@ mod test {
     use super::*;
     use widestring::Utf16String;
 
-    type Dal = DomActionList<Utf16String>;
+    type ActionList = DomActionList<Utf16String>;
 
     #[test]
     fn default_creates_empty_action_list() {
-        let list = Dal::default();
+        let list = ActionList::default();
         assert!(list.actions.is_empty());
     }
 
     #[test]
     fn new_creates_action_list_with_passed_actions() {
         let actions = default_actions();
-        let list = Dal::new(actions.clone());
+        let list = ActionList::new(actions.clone());
         assert_eq!(actions, list.actions);
     }
 
     #[test]
     fn new_can_create_empty_list_too() {
-        let list = Dal::new(Vec::new());
+        let list = ActionList::new(Vec::new());
         assert!(list.actions.is_empty());
     }
 
     #[test]
     fn actions_fn_returns_all_actions() {
         let actions = default_actions();
-        let list = Dal::new(actions.clone());
+        let list = ActionList::new(actions.clone());
         assert_eq!(list.actions()[0], actions[0]);
         assert_eq!(list.actions()[1], actions[1]);
         assert_eq!(list.actions()[2], actions[2]);
@@ -228,7 +228,7 @@ mod test {
     #[test]
     fn additions_returns_only_add_actions() {
         let actions = default_actions();
-        let list = Dal::new(actions.clone());
+        let list = ActionList::new(actions.clone());
         assert_eq!(1, list.additions().len());
         assert_eq!(DomAction::Add(list.additions()[0].clone()), actions[0]);
     }
@@ -236,7 +236,7 @@ mod test {
     #[test]
     fn removals_returns_only_remove_actions() {
         let actions = default_actions();
-        let list = Dal::new(actions.clone());
+        let list = ActionList::new(actions.clone());
         assert_eq!(1, list.removals().len());
         assert_eq!(DomAction::Remove(list.removals()[0].clone()), actions[2]);
     }
@@ -244,7 +244,7 @@ mod test {
     #[test]
     fn moves_returns_only_move_actions() {
         let actions = default_actions();
-        let list = Dal::new(actions.clone());
+        let list = ActionList::new(actions.clone());
         assert_eq!(1, list.moves().len());
         assert_eq!(DomAction::Move(list.moves()[0].clone()), actions[1]);
     }
@@ -252,7 +252,7 @@ mod test {
     #[test]
     fn push_adds_new_action_at_the_end_of_the_list() {
         let actions = default_actions();
-        let mut list = Dal::new(actions);
+        let mut list = ActionList::new(actions);
         let new_action = DomAction::remove_node(DomHandle::new_unset());
         list.push(new_action.clone());
         assert_eq!(list.actions[3].clone(), new_action);
@@ -261,7 +261,7 @@ mod test {
     #[test]
     fn replace_actions_replaces_all_actions_with_new_ones() {
         let actions = default_actions();
-        let mut list = Dal::new(actions.clone());
+        let mut list = ActionList::new(actions.clone());
         list.replace_actions(Vec::new());
         assert!(list.actions.is_empty());
         list.replace_actions(actions.clone());
@@ -274,7 +274,7 @@ mod test {
             DomHandle::from_raw(vec![1, 0]),
             DomHandle::from_raw(vec![0, 0]),
         )];
-        let list = Dal::new(actions);
+        let list = ActionList::new(actions);
         let found_parent =
             list.find_moved_parent_or_self(&DomHandle::from_raw(vec![1, 0, 0]));
         assert_eq!(found_parent.unwrap().0, DomHandle::from_raw(vec![1, 0]));
