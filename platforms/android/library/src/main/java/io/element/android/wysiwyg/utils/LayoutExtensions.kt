6641905171d6bc55bfa89d22b8d5ebbcd425d4be
Jonny Andrew <jonny.andrew@protonmail.com>
2023-01-06 17:27:00 +0000

    Add modifications statement to license

--- platforms/android/library/src/main/java/io/element/android/wysiwyg/utils/LayoutExtensions.kt
+++ platforms/android/library/src/main/java/io/element/android/wysiwyg/utils/LayoutExtensions.kt
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2018 The Android Open Source Project
+ * Modifications Copyright 2022 New Vector Ltd
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
