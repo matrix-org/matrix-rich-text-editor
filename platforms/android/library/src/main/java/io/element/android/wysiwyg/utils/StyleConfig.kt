b0388b0544249de8dd8f1841340062392aeb3b1a
Jonny Andrew <jonny.andrew@protonmail.com>
2023-01-10 10:29:35 +0000

    Add unit annotation for dimension

--- platforms/android/library/src/main/java/io/element/android/wysiwyg/utils/StyleConfig.kt
+++ platforms/android/library/src/main/java/io/element/android/wysiwyg/utils/StyleConfig.kt
@@ -4,12 +4,10 @@ import android.graphics.drawable.Drawable
 import androidx.annotation.Px
 
 internal data class StyleConfig(
-    @Px
-    val bulletGapWidth: Float,
-    @Px
-    val bulletRadius: Float,
-    val inlineCodeHorizontalPadding: Int,
-    val inlineCodeVerticalPadding: Int,
+    @Px val bulletGapWidth: Float,
+    @Px val bulletRadius: Float,
+    @Px val inlineCodeHorizontalPadding: Int,
+    @Px val inlineCodeVerticalPadding: Int,
     val inlineCodeSingleLineBg: Drawable,
     val inlineCodeMultiLineBgLeft: Drawable,
     val inlineCodeMultiLineBgMid: Drawable,
