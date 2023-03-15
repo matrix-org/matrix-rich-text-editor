37cd5b8aaf67e6c76e7a46f91601563d3cf5dd79
Alun Turner <alunt@element.io>
2023-03-15 12:23:10 +0000

    pass suggestion through

--- crates/wysiwyg/src/composer_model/hyperlinks.rs
+++ crates/wysiwyg/src/composer_model/hyperlinks.rs
@@ -58,7 +58,7 @@ where
         self.do_replace_text_in(S::default(), suggestion.start, suggestion.end);
         self.state.start = Location::from(suggestion.start);
         self.state.end = self.state.start;
-        self.set_link_with_text(link, text);
+        self.set_link_with_text(link, text, Some(suggestion));
         self.do_replace_text(" ".into())
     }
 
@@ -88,13 +88,14 @@ where
         &mut self,
         link: S,
         text: S,
+        suggestion: Option<SuggestionPattern>,
     ) -> ComposerUpdate<S> {
         let (s, _) = self.safe_selection();
         self.push_state_to_history();
         self.do_replace_text(text.clone());
         let e = s + text.len();
         let range = self.state.dom.find_range(s, e);
-        self.set_link_in_range(link, range)
+        self.set_link_in_range(link, range, suggestion)
     }
 
     pub fn set_link(&mut self, link: S) -> ComposerUpdate<S> {
@@ -103,13 +104,14 @@ where
 
         let range = self.state.dom.find_range(s, e);
 
-        self.set_link_in_range(link, range)
+        self.set_link_in_range(link, range, None)
     }
 
     fn set_link_in_range(
         &mut self,
         mut link: S,
         range: Range,
+        suggestion: Option<SuggestionPattern>,
     ) -> ComposerUpdate<S> {
         self.add_http_scheme(&mut link);
 
@@ -193,10 +195,10 @@ where
         for (_, s, e) in split_points.into_iter() {
             let range = self.state.dom.find_range(s, e);
             // Create a new link node containing the passed range
-            let inserted = self
-                .state
-                .dom
-                .insert_parent(&range, DomNode::new_link(link.clone(), vec![]));
+            let inserted = self.state.dom.insert_parent(
+                &range,
+                DomNode::new_link(link.clone(), vec![], &suggestion),
+            );
             // Remove any child links inside it
             self.delete_child_links(&inserted);
         }
