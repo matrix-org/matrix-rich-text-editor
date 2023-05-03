e0060fa078edeb1ea3829de9fd667de05f5d341a
David Langley <langley.dave@gmail.com>
2023-05-03 22:26:51 +0100

    try run coverage on ci and upload

--- platforms/android/generate_coverage_report.sh
+++ platforms/android/generate_coverage_report.sh
@@ -5,4 +5,4 @@ cd $DIR
 ./gradlew unitTestsWithCoverage
 ./gradlew instrumentationTestsWithCoverage
 ./gradlew generateCoverageReport
-open build/reports/jacoco/generateCoverageReport/html/index.html
+# open build/reports/jacoco/generateCoverageReport/html/index.html
