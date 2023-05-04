43a25ecc91529a7ef442fdcbdcff651981bd9f11
David Langley <langley.dave@gmail.com>
2023-05-04 23:46:29 +0100

    use run for coverage/report

--- platforms/android/generate_coverage_report.sh
+++ platforms/android/generate_coverage_report.sh
@@ -3,6 +3,6 @@
 DIR=$(dirname -- "$0")
 cd $DIR
 ./gradlew unitTestsWithCoverage
-./gradlew instrumentationTestsWithCoverage
+# ./gradlew instrumentationTestsWithCoverage
 ./gradlew generateCoverageReport
 open build/reports/jacoco/generateCoverageReport/html/index.html
