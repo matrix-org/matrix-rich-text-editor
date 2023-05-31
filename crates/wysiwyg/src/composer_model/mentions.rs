dc8c86398c452688e9d2768d07100eaba2bbfadf
Alun Turner <alunt@element.io>
2023-05-31 12:12:32 +0100

    move to new file, refactor

--- crates/wysiwyg/src/composer_model/mentions.rs
+++ crates/wysiwyg/src/composer_model/mentions.rs
@@ -13,14 +13,15 @@
 // limitations under the License.
 
 use crate::{
-    ComposerModel, ComposerUpdate, DomNode, Location, SuggestionPattern,
-    UnicodeString,
+    dom::Range, ComposerModel, ComposerUpdate, DomNode, Location,
+    SuggestionPattern, UnicodeString,
 };
 
 impl<S> ComposerModel<S>
 where
     S: UnicodeString,
 {
+    /// Replaces the suggestion text with a mention node
     pub fn set_mention_from_suggestion(
         &mut self,
         url: S,
@@ -28,73 +29,80 @@ where
         suggestion: SuggestionPattern,
         attributes: Vec<(S, S)>,
     ) -> ComposerUpdate<S> {
-        // This function removes the text between the suggestion start and end points, updates the cursor position
-        // and then calls set_mention_with_text (for equivalence with stages for inserting a link)
-
+        // This function removes the text between the suggestion start and end points, updates the
+        // cursor position and then calls set_mention (equivalent to link insertion steps)
         self.do_replace_text_in(S::default(), suggestion.start, suggestion.end);
         self.state.start = Location::from(suggestion.start);
         self.state.end = self.state.start;
 
-        self.set_mention_with_text(url, text, attributes)
+        self.set_mention(url, text, attributes)
     }
 
-    fn set_mention_with_text(
+    /// Inserts a mention node into the composer
+    fn set_mention(
         &mut self,
         url: S,
         text: S,
         attributes: Vec<(S, S)>,
     ) -> ComposerUpdate<S> {
-        // this function is similar to set_link_with_text, but now we call a new simpler insertion method
         self.push_state_to_history();
-        self.insert_node_at_cursor(DomNode::new_mention(url, text, attributes))
+        let (s, e) = self.safe_selection();
+
+        let range = self.state.dom.find_range(s, e);
+        let mention_node = DomNode::new_mention(url, text, attributes);
+
+        if range.is_cursor() {
+            self.set_mention_at_cursor(mention_node, range)
+        } else {
+            // TODO confirm behaviour when we want to set a mention with a selection
+            ComposerUpdate::keep()
+        }
     }
 
-    /// Inserts the given node at the current cursor position
-    fn insert_node_at_cursor(&mut self, node: DomNode<S>) -> ComposerUpdate<S> {
-        let (s, e) = self.safe_selection();
-        let range = self.state.dom.find_range(s, e);
-
+    /// Inserts the mention node at the current cursor position
+    fn set_mention_at_cursor(
+        &mut self,
+        mention_node: DomNode<S>,
+        range: Range,
+    ) -> ComposerUpdate<S> {
         // manually determine where the cursor will be after we finish the amendment
-        let new_cursor_position = s + node.text_len();
-
+        let new_cursor_position = range.start() + mention_node.text_len();
         let mut should_add_trailing_space = false;
 
-        // do nothing if we don't have a cursor
-        // TODO determine behaviour if we try to insert when we have a selection
-        if range.is_selection() {
-            ComposerUpdate::<S>::keep();
-        }
-
         // manipulate the state of the dom as required
-        if range.is_empty() {
-            // this happens if we replace the whole of a single text node in the composer
-            self.state.dom.append_at_end_of_document(node);
-            should_add_trailing_space = true;
-        } else if let Some(leaf) = range.leaves().next() {
+        if let Some(leaf) = range.leaves().next() {
             // when we have a leaf, the way we treat the insertion depends on the cursor position inside that leaf
             let cursor_at_end = leaf.start_offset == leaf.length;
             let cursor_at_start = leaf.start_offset == 0;
 
             if cursor_at_start {
                 // insert the new node before a leaf that contains a cursor at the start
-                self.state.dom.insert_at(&leaf.node_handle, node);
+                self.state.dom.insert_at(&leaf.node_handle, mention_node);
             } else if cursor_at_end {
                 // insert the new node after a leaf that contains a cursor at the end
                 let parent_node = self.state.dom.parent(&leaf.node_handle);
-                self.state.dom.append(&parent_node.handle(), node);
+                self.state.dom.append(&parent_node.handle(), mention_node);
                 should_add_trailing_space = true;
             } else {
                 // otherwise insert the new node in the middle of a text node
                 self.state.dom.insert_into_text(
                     &leaf.node_handle,
                     leaf.start_offset,
-                    node,
+                    mention_node,
                 );
             }
         } else {
-            // TODO this is the case where we have some locations, but none of them are leaves
-            // ie we are in a container node
-            panic!("TODO implement inserting mention in container node")
+            // if we haven't found a leaf node, try to find a container node
+            let first_location = range.locations.first();
+
+            match first_location {
+                None => self.state.dom.append_at_end_of_document(mention_node),
+                Some(container) => {
+                    self.state.dom.append(&container.node_handle, mention_node)
+                }
+            };
+
+            should_add_trailing_space = true;
         }
 
         // after manipulation, update cursor position, add a trailing space if desired, return
