431b5603d6d922eadc330497fb6b6d4b8c9a4e46
jonnyandrew <jonnya@element.io>
2023-05-31 13:44:06 +0100

    Fix wasm test

--- crates/wysiwyg/src/dom/parser/parse.rs
+++ crates/wysiwyg/src/dom/parser/parse.rs
@@ -1035,7 +1035,7 @@ mod js {
         #[wasm_bindgen_test]
         fn a_with_attributes() {
             roundtrip(
-                r#"<a data-mention-type="user" style="something" href="http://example.com" contenteditable="false">a user mention</a>"#,
+                r#"<a contenteditable="false" data-mention-type="user" style="something" href="http://example.com">a user mention</a>"#,
             );
         }
 
