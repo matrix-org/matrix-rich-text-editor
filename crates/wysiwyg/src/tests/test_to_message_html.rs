5d9183e1d6f69e30254860b54e2b47a2d1840610
Alun Turner <alunt@element.io>
2023-06-12 14:52:49 +0100

    add new test file

---
+++ crates/wysiwyg/src/tests/test_to_message_html.rs
@@ -0,0 +1,72 @@
+// Copyright 2023 The Matrix.org Foundation C.I.C.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+use crate::tests::testutils_composer_model::{cm, tx};
+
+#[test]
+fn only_outputs_href_attribute_on_user_mention() {
+    let mut model = cm("|");
+    model.insert_mention(
+        "www.url.com".into(),
+        "inner text".into(),
+        vec![
+            ("data-mention-type".into(), "user".into()),
+            ("style".into(), "some css".into()),
+        ],
+    );
+    assert_eq!(tx(&model), "<a data-mention-type=\"user\" style=\"some css\" href=\"www.url.com\" contenteditable=\"false\">inner text</a>&nbsp;|");
+
+    let message_output = model.get_content_as_message_html();
+    assert_eq!(
+        message_output,
+        "<a href=\"www.url.com\">inner text</a>\u{a0}"
+    );
+}
+
+#[test]
+fn only_outputs_href_attribute_on_room_mention() {
+    let mut model = cm("|");
+    model.insert_mention(
+        "www.url.com".into(),
+        "inner text".into(),
+        vec![
+            ("data-mention-type".into(), "room".into()),
+            ("style".into(), "some css".into()),
+        ],
+    );
+    assert_eq!(tx(&model), "<a data-mention-type=\"room\" style=\"some css\" href=\"www.url.com\" contenteditable=\"false\">inner text</a>&nbsp;|");
+
+    let message_output = model.get_content_as_message_html();
+    assert_eq!(
+        message_output,
+        "<a href=\"www.url.com\">inner text</a>\u{a0}"
+    );
+}
+
+#[test]
+fn only_outputs_href_inner_text_for_at_room_mention() {
+    let mut model = cm("|");
+    model.insert_mention(
+        "anything".into(),
+        "@room".into(),
+        vec![
+            ("data-mention-type".into(), "at-room".into()),
+            ("style".into(), "some css".into()),
+        ],
+    );
+    assert_eq!(tx(&model), "<a data-mention-type=\"at-room\" style=\"some css\" href=\"#\" contenteditable=\"false\">@room</a>&nbsp;|");
+
+    let message_output = model.get_content_as_message_html();
+    assert_eq!(message_output, "@room\u{a0}");
+}
