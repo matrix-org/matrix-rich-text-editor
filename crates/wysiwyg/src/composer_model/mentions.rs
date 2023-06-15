afd8efbc1d21a7396742eccc3d59e86f348dd7f1
Alun Turner <alunt@element.io>
2023-06-15 15:36:29 +0100

    refactor

--- crates/wysiwyg/src/composer_model/mentions.rs
+++ crates/wysiwyg/src/composer_model/mentions.rs
@@ -43,7 +43,12 @@ where
         self.do_replace_text_in(S::default(), suggestion.start, suggestion.end);
         self.state.start = Location::from(suggestion.start);
         self.state.end = self.state.start;
-        self.do_insert_mention(url, text, attributes)
+
+        if let Ok(mention_node) = DomNode::new_mention(url, text, attributes) {
+            self.do_insert_mention(mention_node)
+        } else {
+            ComposerUpdate::keep()
+        }
     }
 
     /// Remove the suggestion text and then insert an at-room mention into the composer, using the following rules
@@ -64,7 +69,9 @@ where
         self.do_replace_text_in(S::default(), suggestion.start, suggestion.end);
         self.state.start = Location::from(suggestion.start);
         self.state.end = self.state.start;
-        self.do_insert_at_room_mention(attributes)
+
+        let mention_node = DomNode::new_at_room_mention(attributes);
+        self.do_insert_mention(mention_node)
     }
 
     /// Inserts a mention into the composer. It uses the following rules:
@@ -87,7 +94,12 @@ where
         if self.has_selection() {
             self.do_replace_text(S::default());
         }
-        self.do_insert_mention(url, text, attributes)
+
+        if let Ok(mention_node) = DomNode::new_mention(url, text, attributes) {
+            self.do_insert_mention(mention_node)
+        } else {
+            ComposerUpdate::keep()
+        }
     }
 
     pub fn insert_at_room_mention(
@@ -103,7 +115,8 @@ where
             self.do_replace_text(S::default());
         }
 
-        self.do_insert_at_room_mention(attributes)
+        let mention_node = DomNode::new_at_room_mention(attributes);
+        self.do_insert_mention(mention_node)
     }
 
     /// Creates a new mention node then inserts the node at the cursor position. If creation fails due to
@@ -111,48 +124,18 @@ where
     /// It adds a trailing space when the inserted mention is the last node in it's parent.
     fn do_insert_mention(
         &mut self,
-        url: S,
-        text: S,
-        attributes: Vec<(S, S)>,
+        mention_node: DomNode<S>,
     ) -> ComposerUpdate<S> {
-        let (start, end) = self.safe_selection();
-        let range = self.state.dom.find_range(start, end);
-
-        if let Ok(new_node) = DomNode::new_mention(url, text, attributes) {
-            let new_cursor_index = start + new_node.text_len();
-
-            let handle = self.state.dom.insert_node_at_cursor(&range, new_node);
-
-            // manually move the cursor to the end of the mention
-            self.state.start = Location::from(new_cursor_index);
-            self.state.end = self.state.start;
-
-            // add a trailing space in cases when we do not have a next sibling
-            if self.state.dom.is_last_in_parent(&handle) {
-                self.do_replace_text(" ".into())
-            } else {
-                self.create_update_replace_all()
-            }
-        } else {
-            ComposerUpdate::keep()
+        if !mention_node.is_mention_node() {
+            return ComposerUpdate::keep();
         }
-    }
 
-    /// Creates a new mention node then inserts the node at the cursor position. If creation fails due to
-    /// an invalid uri, it will return `ComposerUpdate::keep()`.
-    /// It adds a trailing space when the inserted mention is the last node in it's parent.
-    fn do_insert_at_room_mention(
-        &mut self,
-        attributes: Vec<(S, S)>,
-    ) -> ComposerUpdate<S> {
         let (start, end) = self.safe_selection();
         let range = self.state.dom.find_range(start, end);
 
-        let new_node = DomNode::new_at_room_mention(attributes);
+        let new_cursor_index = start + mention_node.text_len();
 
-        let new_cursor_index = start + new_node.text_len();
-
-        let handle = self.state.dom.insert_node_at_cursor(&range, new_node);
+        let handle = self.state.dom.insert_node_at_cursor(&range, mention_node);
 
         // manually move the cursor to the end of the mention
         self.state.start = Location::from(new_cursor_index);
