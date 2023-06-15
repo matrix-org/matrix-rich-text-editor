2b81075b4d54c94ad5c6213e0b7b47e943064b9c
Alun Turner <alunt@element.io>
2023-06-15 16:41:31 +0100

    reinstate whitespace

--- crates/wysiwyg/src/dom/nodes/dom_node.rs
+++ crates/wysiwyg/src/dom/nodes/dom_node.rs
@@ -11,6 +11,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
 use crate::composer_model::example_format::SelectionWriter;
 use crate::dom::dom_handle::DomHandle;
 use crate::dom::nodes::{
