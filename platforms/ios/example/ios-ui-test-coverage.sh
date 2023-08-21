2e5145cc49e83f1ff88822949e034a4c49fc7be8
aringenbach <80891108+aringenbach@users.noreply.github.com>
2023-08-21 12:55:14 +0200

    Update xcode version to 14.3.1 (#775)

--- platforms/ios/example/ios-ui-test-coverage.sh
+++ platforms/ios/example/ios-ui-test-coverage.sh
@@ -6,7 +6,7 @@ xcodebuild \
   -project Wysiwyg.xcodeproj \
   -scheme Wysiwyg \
   -sdk iphonesimulator \
-  -destination 'platform=iOS Simulator,name=iPhone 14,OS=16.2' \
+  -destination 'platform=iOS Simulator,name=iPhone 14,OS=16.4' \
   -derivedDataPath ./DerivedData \
   -enableCodeCoverage YES \
   test
