468132722f83a222df5abcd95cf3c637447f6c1c
aringenbach <arnaudr@element.io>
2023-08-23 16:03:06 +0200

    Set Swift library to be dynamic

--- platforms/ios/lib/WysiwygComposer/Package.swift
+++ platforms/ios/lib/WysiwygComposer/Package.swift
@@ -11,6 +11,7 @@ let package = Package(
     products: [
         .library(
             name: "WysiwygComposer",
+            type: .dynamic,
             targets: ["WysiwygComposer"]
         ),
     ],
