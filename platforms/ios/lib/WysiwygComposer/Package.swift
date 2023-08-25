24dc9dcf0cdf86fe12924065f70453af0bfdc595
aringenbach <arnaudr@element.io>
2023-08-25 11:52:35 +0200

    Revert "Set Swift library to be dynamic (#782)"
    This reverts commit 5a2cbeeb80de034701f10722e2f84711872070b4.

--- platforms/ios/lib/WysiwygComposer/Package.swift
+++ platforms/ios/lib/WysiwygComposer/Package.swift
@@ -11,7 +11,6 @@ let package = Package(
     products: [
         .library(
             name: "WysiwygComposer",
-            type: .dynamic,
             targets: ["WysiwygComposer"]
         ),
     ],
