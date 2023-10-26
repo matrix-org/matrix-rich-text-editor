2d180dc7906cbf20d9ab34e26d097e7e8d0807f4
Jorge Martín <jorgem@element.io>
2023-10-26 12:04:28 +0200

    Add missing `onError` arg to `remember`

--- platforms/android/library-compose/src/main/java/io/element/android/wysiwyg/compose/RichTextEditor.kt
+++ platforms/android/library-compose/src/main/java/io/element/android/wysiwyg/compose/RichTextEditor.kt
@@ -167,7 +167,7 @@ private fun RealEditor(
         // The `update` lambda is called when the view is first created, and then again whenever the actual `update` lambda changes. That is, it's replaced with
         // a new lambda capturing different variables from the surrounding scope. However, there seems to be an issue that causes the `update` lambda to change
         // more than it's strictly necessary. To avoid this, we can use a `remember` block to cache the `update` lambda, and only update it when needed.
-        update = remember(style, typeface, mentionDisplayHandler) {
+        update = remember(style, typeface, mentionDisplayHandler, onError) {
             { view ->
                 Timber.d("RealEditor's update block called, recomposing!")
                 view.applyStyleInCompose(style)
