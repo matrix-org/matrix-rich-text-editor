8c743d1914556deb10d09fec406dc508c7e67f14
Jorge Martín <jorgem@element.io>
2023-11-28 16:42:41 +0100

    Make `RustCleanerTask` internal

--- platforms/android/library/src/main/java/io/element/android/wysiwyg/utils/RustCleanerTask.kt
+++ platforms/android/library/src/main/java/io/element/android/wysiwyg/utils/RustCleanerTask.kt
@@ -3,7 +3,7 @@ package io.element.android.wysiwyg.utils
 import timber.log.Timber
 import uniffi.wysiwyg_composer.Disposable
 
-class RustCleanerTask(
+internal class RustCleanerTask(
     private val disposable: Disposable,
 ) : Runnable {
     override fun run() {
