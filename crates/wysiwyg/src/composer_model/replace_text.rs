ffc6f8b84126b59c24ef727fad27eb43a75ef727
Jorge Martín <jorgem@element.io>
2024-01-31 16:48:32 +0100

    Remove NBSP chars in `replace_text` for empty paragraphs

--- crates/wysiwyg/src/composer_model/replace_text.rs
+++ crates/wysiwyg/src/composer_model/replace_text.rs
@@ -207,7 +207,7 @@ where
             let mut slices = text_string.split('\n').peekable();
             while let Some(slice) = slices.next() {
                 let (s, e) = self.safe_selection();
-                if !slice.is_empty() {
+                if !slice.is_empty() && slice != "\u{A0}" {
                     self.do_replace_text_in(S::from(slice), s, e);
                 }
                 if slices.peek().is_some() {
