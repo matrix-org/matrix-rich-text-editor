cd36f59e672a9a28790e24f05124c9782f5b7412
manuroe <manu@matrix.org>
2024-04-11 17:48:25 +0200

    iOS tests: Use xcresultparser instead of slather
    As for Element-X-iOS

--- platforms/ios/example/ios-ui-test-coverage.sh
+++ platforms/ios/example/ios-ui-test-coverage.sh
@@ -8,6 +8,7 @@ xcodebuild \
   -sdk iphonesimulator \
   -destination 'platform=iOS Simulator,name=iPhone 15,OS=17.2' \
   -derivedDataPath ./DerivedData \
+  -resultBundlePath ui-tests.xcresult \
   -enableCodeCoverage YES \
   test
   
