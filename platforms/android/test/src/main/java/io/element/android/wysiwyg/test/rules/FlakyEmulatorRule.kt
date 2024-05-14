951ac87695989497cde8fa219c97352040787d2b
Jorge Martín <jorgem@element.io>
2024-05-14 12:02:36 +0200

    Fix tests

--- platforms/android/test/src/main/java/io/element/android/wysiwyg/test/rules/FlakyEmulatorRule.kt
+++ platforms/android/test/src/main/java/io/element/android/wysiwyg/test/rules/FlakyEmulatorRule.kt
@@ -6,6 +6,11 @@ import org.junit.rules.TestRule
 /**
  * Creates a rule that helps to reduce emulator related flakiness.
  */
-fun createFlakyEmulatorRule(): TestRule = RuleChain
-    .outerRule(RetryOnFailureRule())
-    .around(DismissAnrRule())
+fun createFlakyEmulatorRule(retry: Boolean = true): TestRule = if (retry) {
+    RuleChain
+        .outerRule(RetryOnFailureRule())
+        .around(DismissAnrRule())
+} else {
+    RuleChain
+        .outerRule(DismissAnrRule())
+}
