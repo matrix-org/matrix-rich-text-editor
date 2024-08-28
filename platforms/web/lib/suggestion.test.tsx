4fa23e8077e6ef7e52a571fa08bd819b038911e4
David Langley <davidl@element.io>
2024-08-28 13:24:11 +0100

    lint

--- platforms/web/lib/suggestion.test.tsx
+++ platforms/web/lib/suggestion.test.tsx
@@ -13,8 +13,9 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
-// eslint-disable-next-line camelcase
+
 import init, {
+    // eslint-disable-next-line camelcase
     new_composer_model,
     SuggestionPattern,
 } from '../generated/wysiwyg';
